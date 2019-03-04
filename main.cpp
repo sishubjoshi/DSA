2d7cf63a3098c8c0fcd53994dfa3648a77fafb25
sishubjoshi <shisub1999@gmail.com>
2019-03-04 22:24:54 +0530

    vectors and memory and 2dvectors

--- main.cpp
+++ main.cpp
@@ -42,3 +42,81 @@
 // }
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main() {
+//     int n;
+//     cin>>n;
+
+//     // memset(houses, true, 101);
+//     while(n--) {
+//         bool houses[101];
+
+//         for(int i=0; i<100; i++)houses[i] = true;
+        
+//         int policeHouses, speed, minutes;
+
+//         cin>>policeHouses>>speed>>minutes;
+
+//         int police[policeHouses];
+
+//         for(int i=0; i < policeHouses; i++) cin>>police[i];
+
+//         // cout<<policeHouses<<speed<<minutes;
+//         for(int i=0; i<policeHouses; i++) {
+
+//             int l = police[i] - (speed*minutes);
+
+//             if(l < 0) l = 1;
+
+//             int r = police[i] + (speed*minutes);
+
+//             if(r > 100) r = 100;
+
+//             for(int i=l; i <= r; i++) {
+//                 if(houses[i]) {
+//                     houses[i] = false;
+//                 }
+//             }
+//         }
+
+//         int count = 0;
+//         for(int i=0; i<101; i++) {
+
+//             if(houses[i]) count++;
+//             // cout<<i<<" ";
+//         } 
+
+//         cout<<count<<"\n";
+//     }
+//     return 0;
+// }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef  long long int lli;
+int main() {
+    int n;
+    cin>>n;
+    while(n--) {
+        lli N;
+        int d;
+        cin>>N>>d;
+
+        lli x = N, mod = 0;
+        while(x>0) {
+            mod += x%10
+            x = x/10;
+            x = x*10 + d;
+            if(x < N) {
+                N = x;
+            }
+
+        }
+
+        cout<<N;
+
+    }
+}
