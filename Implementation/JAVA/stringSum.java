50850d84ef773c1bb81cd24d2bdac643a646a6ec
= <=>
2019-06-12 19:34:52 +0530

    more to come

--- Implementation/JAVA/stringSum.java
+++ Implementation/JAVA/stringSum.java
@@ -7,13 +7,13 @@ class stringSum {
             small = "0" + small;
         }
 
-        System.out.println("big" + big);
-        System.out.println("low" + small);
+        // System.out.println("big" + big);
+        // System.out.println("low" + small);
         int sum = 0, carry = 0;
         String res = new String();
         for (int i = big.length() - 1; i >= 0; i--) {
             sum = (int) big.charAt(i) - '0' + (int) small.charAt(i) - '0' + carry;
-            if (sum > 9) {
+            if (sum > 9 && i != 0) {
                 sum %= 10;
                 carry = 1;
 
@@ -32,19 +32,19 @@ class stringSum {
         a = scan.nextLine();
         b = scan.nextLine();
         scan.close();
-        System.out.println(a);
-        System.out.println(b);
+        // System.out.println(a);
+        // System.out.println(b);
 
         int a_l = a.length();
         int b_l = b.length();
 
         int diff = a_l - b_l;
 
-        longSum ls = new longSum();
+        stringSum ls = new stringSum();
 
         if (diff > 0)
             ls.addString(b, a, diff);
         else
-            ls.addString(a, b, diff);
+            ls.addString(a, b, diff * -1);
     }
 }
